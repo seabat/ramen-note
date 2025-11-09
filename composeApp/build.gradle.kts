f967257261260c24ab661e7a4bf2e83be776ac8b
seabat <ryouta.nakasnishi.86@gmail.com>
2025-11-09 22:28:11 +0900

    1.0.1(4) -> 1.0.3(5)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -91,8 +91,8 @@ android {
         applicationId = "dev.seabat.ramennote"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 4
-        versionName = "1.0.1"
+        versionCode = 5
+        versionName = "1.0.3"
     }
     buildFeatures {
         buildConfig = true
